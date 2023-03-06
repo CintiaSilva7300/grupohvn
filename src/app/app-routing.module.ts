ed884e1c8ac966a3dacbea76dddd917ac7e77c08
cintia <cintiasilva7300@gmail.com>
2023-03-06 15:21:52 -0300

    criação de banner e alguns outros ajustes no leyout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './componets/not-found/not-found.component';
+import { HomeComponent } from './screens/home/home.component';
+import { LoginComponent } from './screens/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+
+  {
+    path: '', component: HomeComponent
+  },
+
+  {
+    path: '**', pathMatch: 'full',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './componets/not-found/not-found.component';
+import { HomeComponent } from './screens/home/home.component';
+import { CarouselComponent } from './componets/carousel/carousel.component';
+import { CardComponent } from './componets/card/card.component';
+import { LoginComponent } from './screens/login/login.component';
+import { FooterComponent } from './componets/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    HomeComponent,
+    CarouselComponent,
+    CardComponent,
+    LoginComponent,
+    FooterComponent,
+    // RellaxDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
